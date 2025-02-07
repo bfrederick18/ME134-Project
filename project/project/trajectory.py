1fcfb06837adf2c2d00516e2e8c771373ff9f73c
bfrederick18 <brandon@caltech.edu>
2025-02-06 21:44:27 -0800

    Remove artifacts

--- project/project/trajectory.py
+++ project/project/trajectory.py
@@ -159,47 +159,13 @@ class DemoNode(Node):
         self.state_pub.publish(state)
 
 
-
-    def recvpoint(self, pointmsg):
-        x = pointmsg.x
-        y = pointmsg.y
-        z = pointmsg.z
-        
-        if self.mode is Mode.WAITING:
-            if ((x - 0.7455) ** 2 + (y - 0.04) ** 2 + (z - 0.11) ** 2) ** (1 / 2) < 0.74 and z >= 0.0 and y > 0.0:
-                self.set_pointcmd([x, y, z])
-                self.qgoal = self.newton_raphson(self.pointcmd)
-                self.get_logger().info("qgoal: %r" % self.qgoal)
-                self.set_mode(Mode.POINTING)
-            
-                self.get_logger().info("Running point %r, %r, %r" % (x,y,z))
-            else:
-                self.get_logger().info('Not in the dome. Please try again dummy...')
-        else:
-            self.get_logger().info('Not waiting yet. Please wait dummy...')
-
-
-    def recvpoint_list(self, msg):
-        # self.get_logger().info("Received a list of points: %r" % msg.points)
-        if self.mode is Mode.WAITING:
-            self.point_array.points = msg.points[:]
-
-            if len(self.point_array.points) > 0:
-                first_point = self.point_array.points[0]
-                if ((first_point.x - 0.7455) ** 2 + (first_point.y - 0.04) ** 2 + (first_point.z - 0.11) ** 2) ** 0.5 < 0.74 and first_point.z >= 0.0 and first_point.y > 0.0:
-                    self.set_pointcmd([first_point.x, first_point.y, first_point.z])
-                    self.set_mode(Mode.POINTING)
-                else:
-                    self.get_logger().info('First point not in safety dome. Ignoring...')
-
-
     def recv_segment_array(self, msg):
         self.get_logger().info("Received a list of segments: %r" % msg.segments)
         if self.mode is Mode.WAITING:
             self.segments = msg.segments
 
             self.tcmd = self.t
-            self.pcmd = self.actpos[:]  # use current joint state
+            self.pcmd = self.actpos[:]
             self.vcmd = [0.0, 0.0, 0.0]
 
             self.set_mode(Mode.POINTING)
@@ -225,27 +191,6 @@ class DemoNode(Node):
         tau_shoulder = self.A * sin(theta_sh) + self.B * cos(theta_sh) - 0.1
         return [0.0, tau_shoulder, 0.0]
     
-    
-    def newton_raphson(self, xgoal):
-        xdistance = []
-        qstepsize = []
-        q = self.actpos
-        N = 500
-
-        for i in range(N+1):
-            (x, _, Jv, _) = self.chain.fkin(q)
-            xdelta = (xgoal - x)
-            qdelta = np.linalg.inv(Jv) @ xdelta
-            q = q + qdelta * 0.5
-            xdistance.append(np.linalg.norm(xdelta))
-            qstepsize.append(np.linalg.norm(qdelta))
-
-            if np.linalg.norm(x-xgoal) < 1e-12:
-                self.get_logger().info("Completed in %d iterations" % i)
-                return q.tolist()
-            
-        return WAITING_POS
-
 
     def update(self):
         now = self.get_clock().now()
@@ -264,77 +209,6 @@ class DemoNode(Node):
                 self.set_mode(Mode.WAITING)
 
         elif self.mode is Mode.POINTING:
-            #     # populate self.segments
-
-            #     cart_points = [self.x_waiting]
-            #     for pt in self.point_array.points:
-            #         cart_points.append([pt.x, pt.y, pt.z])
-            #     self.point_array.points = []
-                
-            #     Tmove = CYCLE / 2
-
-            #     for i in range(len(cart_points) - 1):
-            #         p1 = cart_points[i]
-            #         p2 = cart_points[i + 1]
-
-            #         transitional = [(p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2, 0.07]
-
-            #         qT = self.newton_raphson(transitional)
-            #         q2 = self.newton_raphson(p2)
-
-            #         if i == 0:
-            #             a_seg = Segment()
-            #             a_seg.px = q2[0]
-            #             a_seg.py = q2[1]
-            #             a_seg.pz = q2[2]
-            #             a_seg.vx = 0.0
-            #             a_seg.vy = 0.0
-            #             a_seg.vz = 0.0
-            #             a_seg.t = Tmove * 2
-            #             self.segment_array.segments.append(a_seg)
-            #             continue
-
-            #         dx = (transitional[0] - p1[0])
-            #         dy = (transitional[1] - p1[1])
-            #         v_cart = np.array([dx / Tmove, dy / Tmove, 0.0])
-
-            #         (_, _, Jv, _) = self.chain.fkin(qT)
-            #         qdotT = np.linalg.pinv(Jv) @ v_cart
-            #         qdotT = qdotT.flatten().tolist()
-
-            #         seg1 = Segment()
-            #         seg1.px = qT[0]
-            #         seg1.py = qT[1]
-            #         seg1.pz = qT[2]
-            #         seg1.vx = qdotT[0]
-            #         seg1.vy = qdotT[1]
-            #         seg1.vz = qdotT[2]
-            #         seg1.t = Tmove
-            #         # Segment(pf=qT, vf=qdotT, Tmove=Tmove)
-            #         # seg2 = Segment(pf=q2, vf=[0.0, 0.0, 0.0], Tmove=Tmove)
-            #         seg2 = Segment()
-            #         seg2.px = q2[0]
-            #         seg2.py = q2[1]
-            #         seg2.pz = q2[2]
-            #         seg2.vx = 0.0
-            #         seg2.vy = 0.0
-            #         seg2.vz = 0.0
-            #         seg2.t = Tmove
-
-            #         self.segment_array.segments.append(seg1)
-            #         self.segment_array.segments.append(seg2)
-
-            #     a_seg = Segment()
-            #     a_seg.px = WAITING_POS[0]
-            #     a_seg.py = WAITING_POS[1]
-            #     a_seg.pz = WAITING_POS[2]
-            #     a_seg.vx = 0.0
-            #     a_seg.vy = 0.0
-            #     a_seg.vz = 0.0
-            #     a_seg.t = Tmove * 2
-            #     self.segment_array.segments.append(a_seg)
-
-
             if self.spline and ((self.t - self.spline.t0) > self.spline.T or self.abort):
                 self.spline = None
                 self.abort = False
